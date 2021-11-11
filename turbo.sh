d3b5fa0b9eb3f19779e3b1bb3c105c47e1c8d393
FORTBRIDGE <75114098+fortbridge@users.noreply.github.com>
2021-11-11 07:35:51 +0000

    Delete turbo.sh

--- turbo.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-for number in {1..1000}
-do
-	java -jar /home/adrian/.BurpSuite/bapps/9abaa233088242e8be252cd4ff534988/build/libs/turbo-intruder-all.jar ./script.py request.txt http://13.40.10.158  foo
-	sleep 10 
-done
-
